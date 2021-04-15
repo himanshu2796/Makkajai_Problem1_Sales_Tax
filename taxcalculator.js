d1e2fcc5500afa952086a8f0af84b29e6907bba2
himanshu2796 <kumarhimanshu382@gmail.com>
2021-04-15 12:55:10 +0530

    refactored

--- taxcalculator.js
+++ taxcalculator.js
@@ -1,39 +1,59 @@
 class TaxCalculator {
+    importedTax = 5;
+    basicSalesTax = 10;
+    arrayItem = ["book","medicine","food"];
     constructor(number, cost, typeItem, imported) {
         this.calculateItemWithTax(number, cost, typeItem, imported);
     }
 
     calculateItemWithTax = (number, cost, typeItem, imported) => {
-        let costItem = 0;
+        let costItem = cost;
         let individualTax = 0;
-        let arrayItem = ["book","medicine","food"];
-        if (imported === 'Y') {
-            if (arrayItem.includes(typeItem)) {
-                individualTax = Number((Math.ceil((number * cost * 5 / 100) * 20) / 20).toFixed(2));
-                costItem = Number((number * (cost) + individualTax).toFixed(2));
-            }
-            else {
-                individualTax = Number((Math.ceil(number * (cost * 15 / 100) * 20) / 20).toFixed(2));
-                costItem = Number((number * (cost) + individualTax).toFixed(2));
 
-            }
-        }
-        else {
-            if (arrayItem.includes(typeItem)) {
-                costItem = cost;
-                individualTax = 0;
-            }
-            else {
-                individualTax = Number((Math.ceil(number * (cost * 10 / 100) * 20) / 20).toFixed(2));
-                costItem = Number((number * (cost) + individualTax).toFixed(2));
-            }
+        let conditional ={imported:this.isImported(imported),exempted:this.isExempted(typeItem)};
 
-        }
+        let tax = this.TotalSalesTax(conditional["imported"],conditional["exempted"]);
+
+        individualTax = this.calculateIndividualTax(number,cost,tax);
+        costItem = this.calculateCostItem(number,cost,individualTax);
         return { costItem, individualTax };
     }
 
+    calculateIndividualTax = (number,cost,taxvalue)=>{
+         return Number((Math.ceil(number*(cost*taxvalue/100)*20)/20).toFixed(2));
+    }
 
+    calculateCostItem = (number,cost,individualtax)=>{
+        return Number((number * (cost) + individualtax).toFixed(2));
+    }
 
+    isImported = (imported)=>{
+        if(imported==='Y'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    isExempted = (type)=>{
+        if(this.arrayItem.includes(type)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+        }
+     TotalSalesTax(imported,exempted){
+         let tax = 0;
+         if(imported)
+         tax+=this.importedTax;
+         if(!exempted)
+         tax+=this.basicSalesTax;
+
+         return tax;
+     }
 }
 
 module.exports = TaxCalculator;
